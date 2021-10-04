6bda8c1128160cd8231a034e1e66258ee138f30d
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2021-10-04 14:15:46 +0100

    mychanges

--- src/oocweek3/OOCWeek3.java
+++ src/oocweek3/OOCWeek3.java
@@ -15,7 +15,10 @@ public class OOCWeek3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello "
+                + "my "
+                + "world"); 
+    
     }
     
 }
